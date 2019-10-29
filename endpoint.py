f0bccd8147e3add0c020cfd9c5a499b3cd4005aa
Francesca Koulikov <fkoulikov@illuminateed.net>
2019-10-29 10:27:12 -0400

    made some changes for transmitting data, there are errors

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,13 @@
 import logging
 import platform
-from PlatformEndpoint import *
+import PlatformEndpoint
+import DataTransmitter
+import LoggerProvider
 import argparse
+import socket
+from PlatformEndpoint import *
+from DataTransmitter import *
+from LoggerProvider import *
 
 
 class Endpoint:
@@ -11,29 +17,18 @@ class Endpoint:
     def __init__(self):
         pass
 
-    def setPlatformObject(self, logger, handler):
-        self.platformObject = PlatformEndpoint(logger, handler)
-        self.logger = logger
-        self.handler = handler
+    def setLogger(self):
+        self.loggerObject = LoggerProvider()
+        self.logger, self.handler = self.loggerObject.createLogger()
+
+    def setPlatformObject(self):
+        self.platformObject = PlatformEndpoint(self.loggerObject, self.logger, self.handler)
 
     def getArgs(self, argv=None):
         parser = argparse.ArgumentParser(description="Red Canary Endpoint")
         parser.add_argument("start_process", type=str, help="start A process. Takes subarguments")
         return parser.parse_args(argv)
 
-    def createLogger(self):
-        logFormatter = logging.Formatter("%(asctime)s - %(message)s")
-
-        # configure file handler
-        fileHandler = logging.FileHandler('file.log')
-        fileHandler.setFormatter(logFormatter)
-
-        logger = logging.getLogger(__name__)
-
-        logger.setLevel(logging.INFO)
-
-        logger.addHandler(fileHandler)
-        return logger, fileHandler
 
     def startProcess(self, processName, processCommand=None):
         # creating a custom logger
@@ -47,31 +42,53 @@ class Endpoint:
         elif descriptor.lower() == "delete":
             self.platformObject.deleteAFile(filePath, filename)
         else:
-            logger.warning("your descriptor %s does not exist, please either provide create, modify or delete as a descriptor" % descriptor)
+            self.logger.warning("your descriptor %s does not exist, please either provide create, modify or delete as a descriptor" % descriptor)
 
-    def start_connections(self, host, port, data, num_conns=1):
-        self.platformObject.transmitData(host, port, num_conns, data)
-
-
+    def transmitData(self, host, port, data, protocol='tcp'):
+        # DataTransmitter object
+        msgLen = len(data)
+        sourceHostName = socket.gethostbyname()
+        sourceHost = socket.gethostbyname(sourceHostName)
+        dataTransmissionCustomFormatterDict = {
+            'destinationAddress': host,
+            'destinationPort': port,
+            'sourceAddress': sourceHost,
+            'sourcePort': None,
+            'protocol': protocol,
+            'amount': msgLen,
+            'action': 'network'
+        }
+        self.loggerObject.setCustomLoggerFormatter(extraParameters=dataTransmissionCustomFormatterDict)
+        dataTransmitterObject = DataTransmitter(host, port, self.logger)
+        socket = dataTransmitterObject.setSock()
+        sourceHost, sourcePort = socket.getsockname()
+        dataTransmissionCustomFormatterDict['sourcePort'] = sourcePort
+        self.loggerObject.setCustomLoggerFormatter(extraParameters=dataTransmissionCustomFormatterDict)
+        dataTransmitterObject.connect()
+    
 # beginning of program
 if __name__ == "__main__":
     # create endpoint object
     endpoint = Endpoint()
 
     # create logger
-    print "going to create logger"
-    logger, handler = endpoint.createLogger()
+    endpoint.setLogger()
 
     # set platformEndpoint object
-    endpoint.setPlatformObject(logger, handler)
+    #endpoint.setPlatformObject(logger, handler)
 
     # testing starting a process
     #endpoint.startProcess("python")
 
     # testing creating a file
-    endpoint.fileManipulator("create", "/Users/francescakoulikov/red-canary", "test.txt")
+    #endpoint.fileManipulator("create", "/Users/francescakoulikov/red-canary", "test.txt")
 
     # testing deleting a file
-    endpoint.fileManipulator("modify", "/Users/francescakoulikov/red-canary", "test.txt", data="what am I to do")
+    #endpoint.fileManipulator("modify", "/Users/francescakoulikov/red-canary", "test.txt", data="what am I to do")
 
+    # Testing transmitting data
+    host = "https://postman-echo.com/post"
+    port = 443
+    data = [b'Message 1 from frany.', b'Message 2 from frany.']
+    endpoint.transmitData(host, port, data)
 
